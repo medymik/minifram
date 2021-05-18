c0c5a442f74cfde8639ddb14777bfd24c36066d1
medymik <mohammed.ymik@outlook.com>
2021-05-18 12:56:41 +0100

    update Router class, router can accept middlewares as a third parameter

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -10,48 +10,58 @@ class Router
 		$this->request = new Request($_SERVER);
 	}
 
-	public function get(string $path, $callback)
+	private function execMiddlewares($middlewares) {
+		foreach($middlewares as $middleware) {
+			$middleware($this);
+		}
+	}
+
+	public function get(string $path, $callback, $middlewares = [])
 	{
 		if ($this->request->getMethod() != 'GET')
 			return;
 		if ($this->request->routeMatch(explode('/', $path)))
 		{
+			$this->execMiddlewares($middlewares);
 			$response = $callback($this->request);
 			echo $response;
 			exit;
 		}
 	}
 
-	public function post(string $path, $callback)
+	public function post(string $path, $callback, $middlewares = [])
 	{
 		if ($this->request->getMethod() != 'POST')
 			return;
 		if ($this->request->routeMatch(explode('/', $path)))
 		{
+			$this->execMiddlewares($middlewares);
 			$response = $callback($this->request);
 			echo $response;
 			exit;
 		}
 	}
 
-	public function put(string $path, $callback)
+	public function put(string $path, $callback, $middlewares = [])
 	{
 		if ($this->request->getMethod() != 'PUT')
 			return;
 		if ($this->request->routeMatch(explode('/', $path)))
 		{
+			$this->execMiddlewares($middlewares);
 			$response = $callback($this->request);
 			echo $response;
 			exit;
 		}
 	}
 
-	public function delete(string $path, $callback)
+	public function delete(string $path, $callback, $middlewares = [])
 	{
 		if ($this->request->getMethod() != 'DELETE')
 			return;
 		if ($this->request->routeMatch(explode('/', $path)))
 		{
+			$this->execMiddlewares($middlewares);
 			$response = $callback($this->request);
 			echo $response;
 			exit;
